70b3c4beaf598b2d75ddd29089569aa4f5c5d71b
u211-10 <u211-10@public.mpei.local>
2021-04-19 12:37:39 +0300

    code: svg module completed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "histogram.h"
-
+#include "svg.h"
 using namespace std;
 const size_t SCREEN_WIDTH = 80;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
@@ -14,7 +14,6 @@ input_numbers(size_t count) {
     }
     return result;
 }
-
 vector<size_t>
 make_histogram(vector<double> numbers,size_t bin_count){
 double min,max;
@@ -42,27 +41,6 @@ size_t number_count=numbers.size();
     return bins;
 }
 void
-svg_begin(double width, double height) {
-    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
-    cout << "<svg ";
-    cout << "width='" << width << "' ";
-    cout << "height='" << height << "' ";
-    cout << "viewBox='0 0 " << width << " " << height << "' ";
-    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
-}
-
-void
-svg_end() {
-    cout << "</svg>\n";
-}
-void
-svg_text(double left, double baseline, string text) {
-    cout << "<text x='" << left << "' y='" <<baseline<< "'>" << text << "</text>";
-}
-void svg_rect(double x, double y, double width, double height,string stroke = "black", string fill = "red"){
-    cout << "<rect x='" << x << "' y='" <<y<< "' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"'></rect>";
-}
-void
 show_histogram_text(vector<size_t> bins){
 
     size_t bin_count=bins.size();
@@ -79,30 +57,6 @@ cout<<"*";
 
 
 }
-
-
-void
-show_histogram_svg(const vector<size_t>& bins){
-const auto IMAGE_WIDTH = 400;
-const auto IMAGE_HEIGHT = 300;
-const auto TEXT_LEFT = 20;
-const auto TEXT_BASELINE = 20;
-const auto TEXT_WIDTH = 50;
-const auto BIN_HEIGHT = 30;
-const auto BLOCK_WIDTH = 10;
-const auto color="green";
-const auto fill="#ffeeee";
-    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-    double top = 0;
-for (size_t bin : bins) {
-    const double bin_width = BLOCK_WIDTH * bin;
-    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
-    top += BIN_HEIGHT;
-}
-    svg_end();
-}
-
 
 
 int main()
