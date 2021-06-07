e8377a6e0336de5d54aa4798e716539bd73c35b8
u211-10 <u211-10@public.mpei.local>
2021-06-07 11:24:06 +0300

    code: добавил curl easy

--- histogram.cpp
+++ histogram.cpp
@@ -1,4 +1,8 @@
 #include "histogram.h"
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
 void
 find_minmax(vector<double> numbers, double& min, double& max)  {
     min = numbers[0];max=numbers[0];
--- main.cpp
+++ main.cpp
@@ -2,40 +2,63 @@
 #include <vector>
 #include "histogram.h"
 #include "svg.h"
+#include <curl/curl.h>
 using namespace std;
 const size_t SCREEN_WIDTH = 80;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
 
 vector<double>
-input_numbers(size_t count) {
+input_numbers(istream& in, size_t count) {
+
     vector<double> result(count);
     for (size_t i = 0; i < count; i++) {
-        cin >> result[i];
+        in >> result[i];
     }
     return result;
 }
+
+Input
+read_input(istream& in,bool prompt) {
+    Input data;
+    if(prompt) cout<<"HELP"<<endl;
+    cerr << "Enter number count: ";
+    size_t number_count;
+    in >> number_count;
+
+    cerr << "Enter numbers: ";
+    data.numbers = input_numbers(in, number_count);
+
+    cerr<<"Enter bin count:";
+    in>>data.bin_count;
+
+    return data;
+}
 vector<size_t>
-make_histogram(vector<double> numbers,size_t bin_count){
+make_histogram(Input a){
 double min,max;
-find_minmax(numbers,min,max);
-double bin_size= (max-min)/bin_count;
+find_minmax(a.numbers,min,max);
+double bin_size= (max-min)/a.bin_count;
     double max_count=0;
 
-vector<size_t> bins(bin_count);
-size_t number_count=numbers.size();
+vector<size_t> bins(a.bin_count);
+size_t number_count=a.numbers.size();
     for (size_t i=0;i<number_count;i++){
         bool found = false;
-    for (size_t j=0;(j<bin_count) && !found;j++){
+    for (size_t j=0;(j<a.bin_count) && !found;j++){
             auto low=min+j*bin_size;
             auto hi=min+(1+j)*bin_size;
-        if((numbers[i]>=low)&&(numbers[i]<hi)){
+        if((a.numbers[i]>=low)&&(a.numbers[i]<hi)){
             bins[j]++;
             found=true;
            }
            if (bins[j]>max_count) max_count=bins[j];
         }
         if (!found) {
-        bins[bin_count - 1]++;
+        bins[a.bin_count - 1]++;
     }
     }
     return bins;
@@ -59,15 +82,19 @@ cout<<"*";
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
-    size_t number_count, bin_count;
-    cerr<<"Enter number count:";
-    cin>>number_count;
-    const auto numbers = input_numbers(number_count);
-    cerr<<"Enter bin count:";
-    cin>>bin_count;
-    const auto bins = make_histogram(numbers, bin_count);
+    if (argc>1){
+    curl_global_init(CURL_GLOBAL_ALL);
+    CURL *curl = curl_easy_init();
+if(curl) {
+  CURLcode res;
+  curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+  res = curl_easy_perform(curl);
+  curl_easy_cleanup(curl);
+}
+    }
+    const auto input = read_input(cin,true);
+    const auto bins = make_histogram(input);
     show_histogram_svg(bins);
-    return 0;
 }
